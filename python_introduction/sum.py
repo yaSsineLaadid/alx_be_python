2ca32e1394e07d1057e55101d4fd8ed48677bed1
Yassine Laadid <yassinelaadid12@gmail.com>
2024-09-19 00:52:16 +0100

    sum.py added to github

---
+++ python_introduction/sum.py
@@ -0,0 +1,13 @@
+# List of numbers
+numbers = [1, 3, 8, 7, 5, 4, 3, 2]
+
+# Initialize total to 0
+total = 0
+
+# Use a for loop to iterate over the numbers
+for number in numbers:
+    total += number  # Add the current number to total
+
+# After the loop, print the final sum
+print("The total sum is:", total)
+
